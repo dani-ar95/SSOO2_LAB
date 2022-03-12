3dd8a6acd87105a8a367b7a62bc558a3e29c9d54
Daniel <Daniel.Almansa1@alu.uclm.es>
2022-03-12 18:29:55 +0100

    Código funcional

--- Lab1/manager.c
+++ Lab1/manager.c
@@ -8,6 +8,8 @@
 #define READ 0
 #define WRITE 1
 
+pid_t array[3];
+
 void signal_handler();
 void install_signal_handler();
 void crear_demonio();
@@ -36,6 +38,7 @@ int main()
 
     FILE *log = fopen("log.txt", "w");
     fprintf(log, "******* Log del sistema ********\n");
+    fclose(log);
 
     if ((pidA = fork()) == -1)
     {
@@ -48,11 +51,13 @@ int main()
         fprintf(stderr, "Error ejecutando el proceso PA: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+    array[0] = pidA;
 
     codigo = wait(&estado);
+    array[0] = 0;
+    log = fopen("log.txt", "a");
     fprintf(log, "Directorios creados\n");
-
-    sleep(3);
+    fclose(log);
 
     if ((pidB = fork()) == -1)
     {
@@ -65,6 +70,7 @@ int main()
         fprintf(stderr, "Error ejecutando el proceso PB: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+    array[1] = pidB;
     if ((pidC = fork()) == -1)
     {
         fprintf(stderr, "Error creando el proceso PC: %s.\n", strerror(errno));
@@ -77,6 +83,7 @@ int main()
         fprintf(stderr, "Error ejecutando el proceso PC: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+    array[2] = pidC;
 
     close(pipeHP[WRITE]);
     read(pipeHP[READ], nota_media, sizeof(nota_media));
@@ -87,18 +94,25 @@ int main()
 
         if (codigo == pidB)
         {
+            array[1] = 0;
+            log = fopen("log.txt", "a");
             fprintf(log, "Copia de modelos de examen, finalizada.\n");
+            fclose(log);
         }
         else
         {
+            array[2] = 0;
+            log = fopen("log.txt", "a");
             fprintf(log, "Creación de archivos con nota necesaria para alcanzar la nota de corte, finalizada.\n");
             fprintf(log, "La nota media de la clase es: %s\n", nota_media);
+            fclose(log);
         }
     }
 
+    log = fopen("log.txt", "a");
     fprintf(log, "FIN DE PROGRAMA");
-
     fclose(log);
+
     return 0;
 }
 
@@ -109,6 +123,16 @@ void signal_handler()
     char *const envParms[] = {NULL};
 
     // terminar procesos activos
+    for(int i = 0; i < 3; i++)
+    {
+        if (array[i] != 0)
+        {
+            if (kill(array[i], SIGINT) == 1)
+            {
+                fprintf(stderr, "Error intentando matar al proceso con pid %d: %s.\n", array[i], strerror(errno));
+            }
+        }
+    }
 
     if ((pidD = fork()) == -1)
     {
@@ -124,6 +148,7 @@ void signal_handler()
     FILE *log = fopen("log.txt", "a");
     fprintf(log, "Interrupción voluntaria CTRL+C");
     fclose(log);
+    exit(EXIT_SUCCESS);
 }
 
 void install_signal_handler()
@@ -149,7 +174,7 @@ void crear_demonio()
     else if (pidDemon == 0)
     {
         execve("daemon", parmList, envParms);
-        fprintf(stderr, "Error ejecutando el proceso PD: %s.\n", strerror(errno));
+        fprintf(stderr, "Error ejecutando el demonio: %s.\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
 }
